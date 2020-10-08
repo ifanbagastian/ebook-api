eeadc54af1c50c08ec18b1a4711b85cd1c9eec33
ifanbagastian <bagastianfauzy@gmail.com>
2020-10-08 10:51:30 +0700

    Upload Auth

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
